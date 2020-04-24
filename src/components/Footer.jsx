1df4835978483005e3d1942a417e95ce48d6195e
Devendra Singh <devsinghindra@gmail.com>
2020-04-24 16:53:05 +0530

    added copyright symbol

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 function Footer() {
-    const date = new Date().getFullYear();
-    return <footer><p>Copyright {date}.</p></footer>
+    const year = new Date().getFullYear();
+    return <footer><p>Copyright ⓒ {year}.</p></footer>
 }
 
 export default Footer;
