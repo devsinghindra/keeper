66ca2379efed07783171c3159816caecf353130c
Devendra Singh <devsinghindra@gmail.com>
2020-04-24 16:53:34 +0530

    added functionality for deleting notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Note() {
-    return <div className="note">
-        <h1>This is Title.</h1>
-        <p>This is Content.</p>
-    </div>;
+function Note(props) {
+
+    function onClick() {
+        props.onDelete(props.id);
+    }
+    return (
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <button onClick={onClick}>DELETE</button>
+        </div>
+    );
 }
 
 export default Note;
