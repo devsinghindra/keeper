f9146d5ecf4194329e059d165862e4ec32d3dfca
Devendra Singh <devsinghindra@gmail.com>
2020-04-24 16:54:09 +0530

    displaying list of notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,35 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return <div>
-        <Header />
-        <Note />
-        <Footer />
-    </div>;
+    const [notes, setNotes] = React.useState([]);
+
+    function onAdd(note) {
+        setNotes(() => {
+            return [...notes, note];
+        })
+    }
+
+    function onDelete(index) {
+        setNotes(notes.filter((note, i) => {
+            return i !== index;
+        }));
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea
+                onAdd={onAdd}
+            />
+            {notes.map((note, index) => {
+                return (<Note key={index} id={index} onDelete={onDelete} title={note.title} content={note.content} />);
+            })}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
